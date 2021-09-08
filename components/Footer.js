5daef1981581d53b52b6f8f8c1f2c9ef762a412f
Eduzin0514 <73307159+Eduzin0514@users.noreply.github.com>
2021-09-08 07:39:17 -0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,21 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
-      </footer>
+      <footer className={styles.footer} style="background-color: #313131; text-align: end; color: white; margin: 0px; padding: 50px 100px 50px 10px; max-width: 100%; border-radius: 10px;"
+			id="contact-tab">
+			<div class="row align-items-center">
+				<div class="col">
+					One of three columns <br><br> oifewf <br><br> aiaiaiaaiiai
+				</div>
+				<div class="col">
+					One of three columns <br><br> oifewf <br><br> aiaiaiaaiiai
+				</div>
+				<div class="col">
+					One of three columns <br><br> oifewf <br><br> aiaiaiaaiiai
+				</div>
+				<script src="js/jquery.js"></script>
+				<script scr="js/bootstrap.bundle.min.js"></script>
+		</footer>
     </>
   )
 }
